3bcc8f01b506a067d59a86eedce3c770048f1b66
unknown <shivthej8@gmail.com>
2025-04-11 23:00:57 +0530

    string reverse

---
+++ test2.py
@@ -0,0 +1,12 @@
+string = "geeks quiz practice code"
+
+# reversing words in a given string
+s = string.split()[::-1]
+l = []
+
+for i in s:
+    # appending reversed words to l
+    l.append(i)
+
+# printing reverse words
+print(" ".join(l))
